6612d51822aa10999844c7cbff581e7d56636947
Bk Lodhi <112323155+itskridhna@users.noreply.github.com>
2022-09-18 00:49:59 -0700

    upload project

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({extended : false}));
 app.use("/api", router);
 
 if(process.env.NODE_ENV == "production"){
-	app.use(express.static(path.join(__dirname,"client/build")))
+	app.use(express.static(path.resolve(__dirname,"client","build")))
 	app.get("/",(req, res)=>{
 		res.sendFile(path.resolve(__dirname,"client","build","index.html"))
 	})
